bdb34ee4d8345a9b6f96b31c1ff229ae328af52c
Thiago Zambonini <Thiagozambonini24@gmail.com>
2023-07-06 15:44:32 -0300

    cors add fix

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const app = express()
 const PORT = process.env.PORT || 9000
 
 //MIDDLEWARES
+app.use(cors({
+  origin: "http://localhost:3000/",
+  credentials: true, 
+  methods: "GET, POST, OPTIONS, PUT, DELETE", 
+  allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept"
+}));
 app.use(express.json())
 app.use(morgan("dev"))
 app.use("/", router)
 app.listen(PORT, () => console.log(`listen on port ${PORT}`))
-app.use(cors({
-    origin: "http://localhost:3000/", // Actualiza con el dominio desde el que harás la solicitud
-    credentials: true, // Permite enviar y recibir cookies en las solicitudes
-    methods: "GET, POST, OPTIONS, PUT, DELETE", // Métodos HTTP permitidos
-    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept" // Encabezados permitidos
-}));
 
 //MONGODB CONNECTION
 require('./db/index.db.js');
