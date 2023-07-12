d6ee7b54cd93c6fe56ebce70be2701abcb8678ee
thiago <thiagozambonini24@gmail.com>
2023-07-12 16:18:41 -0300

    [fix/user properties]

--- src/controllers/user/createUser.controller.js
+++ src/controllers/user/createUser.controller.js
@@ -1,7 +1,7 @@
 const userSchema = require('../../db/models/user.model')
 const mongoose = require('mongoose')
 
-const createUser = async (fullName, username, dateOfBirth, email, password, country, description, profileImage, interactions, accountType) => {
+const createUser = async (fullName, username, dateOfBirth, email, password, country, description, profileImage, interactions, accountType, registerWith) => {
     try {
         const user = await userSchema({
             fullName, 
@@ -13,6 +13,7 @@ const createUser = async (fullName, username, dateOfBirth, email, password, coun
             accountType: accountType || 'user',
             description: description || 'Talent forge user',
             profileImage: profileImage || 'null',
+            registerWith: registerWith || 'local',
             interactions: interactions || {
                 friends:[],
                 savedCourses:[],
--- src/controllers/user/editUser.controller.js
+++ src/controllers/user/editUser.controller.js
@@ -1,9 +1,9 @@
 const userSchema = require('../../db/models/user.model')
 const mongoose = require('mongoose')
 
-const editUsers = async (id, fullName, username, dateOfBirth, email, password, country, description, profileImage, interactions, accountType) => {
+const editUsers = async (id, fullName, username, dateOfBirth, email, password, country, description, profileImage, accountType) => {
     try{ 
-        user = userSchema.updateOne({_id: id}, {$set: {fullName, username, dateOfBirth, email, password, country, description, profileImage, interactions, accountType}})
+        user = userSchema.updateOne({_id: id}, {$set: {fullName, username, dateOfBirth, email, password, country, description, profileImage, accountType}})
         return user
     } catch (error) {
         console.log(error.message) 
