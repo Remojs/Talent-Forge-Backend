ef6f3c678170fbba309766cf278313e31ac07d87
Thiago Zambonini <Thiagozambonini24@gmail.com>
2023-07-13 11:40:12 -0300

    login fix token

--- src/handlers/login/login.handler.js
+++ src/handlers/login/login.handler.js
@@ -1,10 +1,12 @@
 const loginController = require('../../controllers/login/login.controller')
+const jwt = require('jsonwebtoken');
 
 const loginHandler = async(req, res) => {
     try {
         const { username, password } = req.body;
         const logged = await loginController(username, password)
-        res.status(200).json(logged)
+        const decodedToken = jwt.decode(logged)
+        res.status(200).json(decodedToken)
     } catch (error) {
         res.status(500).send({error: error.message})
     }
