d0d2dd8c4499c2d66dfc8b9c94d149e6d276e3e3
thiago <thiagozambonini24@gmail.com>
2023-07-25 11:53:06 -0300

    feat/addPurchasedCourses

---
+++ src/handlers/user/addPurchasedCourses.handler.js
@@ -0,0 +1,19 @@
+const addPurchasedCourses = require('../../controllers/user/addPurchasedCourses.controller')
+const mongoose = require('mongoose')
+
+const addPurchasedCoursesHandler = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const course = {
+            title: req.body.title,
+            id: req.body.id,
+            teacher: req.body.teacher
+        }
+        const addedCourse = await addPurchasedCourses(id, course)
+        res.status(200).json(addedCourse)
+    } catch (error) {
+        res.status(500).send({error: error.message})
+    }
+}
+
+module.exports = addPurchasedCoursesHandler
